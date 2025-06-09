028d1c2778eeffd6e6fd3ebb385b30c17f64813c
AbnormalMatrix <babin.carter@gmail.com>
2025-06-09 11:26:42 -0600

    Add some instructions on the home page

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -10,6 +10,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Home");
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '📏 Tube Cutter',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 12),
+          const Text(
+              'Tube Cutter is a tool for generating simple G‑code to cut square tubing on a CNC plasma cutter.'
+              ''),
+          const SizedBox(height: 8),
+          const Text(
+            '🛠️ Usage:',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 4),
+          const Text('• Define parameters in the setup tab'),
+          const Text('• Preview G-code in the preview tab'),
+          const Text('• Connect to the machine and run in the run job tab'),
+        ],
+      ),
+    );
+    ;
   }
 }
